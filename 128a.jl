d4418927f2d8dc81b54cc7e53394c52aec096691
1s22s1 <1s22s1@users.noreply.github.com>
2024-09-22 13:24:52 +0900

    Solve

---
+++ 128a.jl
@@ -0,0 +1,22 @@
+function parseint()
+    return readline() |> x -> parse(Int, x)
+end
+function parsestring()
+    return readline()
+end
+
+function parseints()
+    return readline() |> split |> x -> parse.(Int, x)
+end
+
+function parsestrings()
+    return readline() |> split
+end
+
+function solve()
+    a, p = parseints()
+
+    println((3a + p) ÷ 2)
+end
+
+solve()
