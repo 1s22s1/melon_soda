927e711201c1fbfdd68855a2c7757afffc01c932
1s22s1 <1s22s1@users.noreply.github.com>
2024-09-22 09:31:12 +0900

    Solve

---
+++ 064a.jl
@@ -0,0 +1,26 @@
+function parseint()
+    return readline() |> x -> parse(Int, x)
+end
+function parsestring()
+    return readline()
+end
+
+function parseints()
+    return readline() |> split |> x -> parse.(Int, x)
+end
+
+function parsestrings()
+    return readline() |> split
+end
+
+function solve()
+    r, g, b = parseints()
+
+    if((100r + 10g + b) % 4 == 0)
+        println("YES")
+    else
+        println("NO")
+    end
+end
+
+solve()
