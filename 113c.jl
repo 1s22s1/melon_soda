307ab91bbaa0b4e610698c685bbea2589a30eb0f
1s22s1 <1s22s1@users.noreply.github.com>
2024-09-28 10:44:35 +0900

    Solve

---
+++ 113c.jl
@@ -0,0 +1,37 @@
+function parseint()
+    return readline() |> x -> parse(Int, x)
+end
+function parsestring()
+    return readline()
+end
+
+function parseints()
+    return readline() |> split |> x -> parse.(Int, x)
+end
+
+function parsestrings()
+    return readline() |> split
+end
+
+function solve()
+    n, m = parseints()
+    municipalities = [parseints() for _ ∈ 1:m]
+
+    dict = Dict()
+
+    for (p, y) ∈ municipalities
+        if haskey(dict, p)
+            dict[p] = sort(push!(dict[p], y))
+        else
+            dict[p] = [y]
+        end
+    end
+
+    for (p, y) ∈ municipalities
+        index = findfirst(x -> x == y, dict[p])
+
+        println(lpad(p, 6, '0'), lpad(index, 6, '0'))
+    end
+end
+
+solve()
