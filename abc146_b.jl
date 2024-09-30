f4b674331ea96589c4ca28053e3e0f14c2c52ae9
1s22s1 <1s22s1@users.noreply.github.com>
2024-10-01 07:28:50 +0900

    Solve

---
+++ abc146_b.jl
@@ -0,0 +1,23 @@
+function parseint()
+    return readline() |> x -> parse(Int, x)
+end
+function parsestring()
+    return readline()
+end
+
+function parseints()
+    return readline() |> split |> x -> parse.(Int, x)
+end
+
+function parsestrings()
+    return readline() |> split
+end
+
+function solve()
+    n = parseint()
+    s = parsestring()
+
+    alphabets =  repeat(collect('A':'Z'),2)
+end
+
+solve()
